324d3b0537c77fa5d38f020bd606a9e4c5558501
Djamilson <djamilson@gmail.com>
2019-04-15 00:34:28 -0300

    Mudança da URL Servidor

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://upload-banckend.herokuapp.com',
+    baseURL: 'https://upload-arquivos-banckend.herokuapp.com',
 
     //baseURL: 'http://localhost:3333',
 });
