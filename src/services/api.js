77baf8e790477efcb4883b1c775e6cba25d0b805
Djamilson <djamilson@gmail.com>
2019-04-16 10:28:43 -0300

    Estrutura do frontend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://upload-arquivos-banckend.herokuapp.com',
+   // baseURL: 'https://upload-arquivos-backend.herokuapp.com',
+    baseURL: 'upload-arquivos-backend.herokuapp.com',
 
    // baseURL: 'http://localhost:3333',
 });
